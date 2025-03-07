380f90d7ca8d064af510aca80c3dcd6b0c181fbc
Eugene Cohen <coheneu@oregonstate.edu>
2025-03-07 09:31:40 -0800

    tests: add chi-arm-thread test
    Add a test configuration to run the thread test
    program on the ARM architecture with the CHI
    coherence protocol.

---
+++ tests/gem5/chi_protocol/configs/chi-arm-thread.py
@@ -0,0 +1,111 @@
+# Copyright (c) 2024 The Regents of the University of California
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met: redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer;
+# redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution;
+# neither the name of the copyright holders nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+A script to run the CHI protocol with different, user-specified, ISA targets
+and number of cores. Sensible SE workloads are used for each supported ISA.
+"""
+
+import argparse
+import os
+
+from gem5.coherence_protocol import CoherenceProtocol
+from gem5.components.boards.simple_board import SimpleBoard
+from gem5.components.cachehierarchies.chi.private_l1_cache_hierarchy import (
+    PrivateL1CacheHierarchy,
+)
+from gem5.components.memory import SingleChannelDDR3_1600
+from gem5.components.processors.cpu_types import CPUTypes
+from gem5.components.processors.simple_processor import SimpleProcessor
+from gem5.isas import (
+    ISA,
+    get_isa_from_str,
+)
+from gem5.resources.resource import BinaryResource
+from gem5.simulate.simulator import Simulator
+from gem5.utils.requires import requires
+
+parser = argparse.ArgumentParser(
+    description="A script to run the CHI protocol with different, "
+    "user-specified, ISA targets and number of codes. Sensible SE workloads "
+    " are used for each supported ISA target. "
+)
+
+parser.add_argument(
+    "--num-cores",
+    type=int,
+    default=1,
+    required=False,
+    help="The number of CPU cores.",
+)
+
+parser.add_argument(
+    "--arguments",
+    type=str,
+    action="append",
+    default=[],
+    required=False,
+    help="The input arguments for the binary.",
+)
+
+args = parser.parse_args()
+
+requires(
+    isa_required=ISA.ARM, coherence_protocol_required=CoherenceProtocol.CHI
+)
+
+cache_hierarchy = PrivateL1CacheHierarchy(size="512KiB", assoc=8)
+
+memory = SingleChannelDDR3_1600(size="32MiB")
+
+processor = SimpleProcessor(
+    cpu_type=CPUTypes.TIMING,
+    isa=ISA.ARM,
+    num_cores=args.num_cores,
+)
+
+board = SimpleBoard(
+    clk_freq="3GHz",
+    processor=processor,
+    memory=memory,
+    cache_hierarchy=cache_hierarchy,
+)
+
+thispath = os.path.dirname(os.path.realpath(__file__))
+bin_path = os.path.join(
+    thispath,
+    "../../..",
+    "test-progs/threads/bin/arm/linux/threads",
+)
+
+# construct the binary resource for the threads app
+binary_res = BinaryResource(local_path=bin_path)
+
+# SimpleBoard inhereits from SEBinaryWorkload
+board.set_se_binary_workload(binary=binary_res, arguments=args.arguments)
+
+simulator = Simulator(board=board)
+simulator.run()
