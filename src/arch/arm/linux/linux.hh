8359723f00444b468324b7dc542c563eafd36c90
Eugene Cohen <coheneu@oregonstate.edu>
2025-03-07 09:41:34 -0800

    arch-arm: Enable clone3 syscall in arm64

--- src/arch/arm/linux/linux.hh
+++ src/arch/arm/linux/linux.hh
@@ -506,6 +506,21 @@ class ArmLinux64 : public ArmLinux, public OpenFlagTable<ArmLinux64>
         uint32_t mem_unit;  /* Memory unit size in bytes */
     };
 
+    struct tgt_clone_args
+    {
+        uint64_t flags;
+        uint64_t pidfd;
+        uint64_t child_tid;
+        uint64_t parent_tid;
+        uint64_t exit_signal;
+        uint64_t stack;
+        uint64_t stack_size;
+        uint64_t tls;
+        uint64_t set_tid;
+        uint64_t set_tid_size;
+        uint64_t cgroup;
+    };
+
     /// For getrusage().
     struct rusage
     {
