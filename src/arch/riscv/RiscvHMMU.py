207be9e6fd8c6feecdc09fd1773f23934287b000
Eugene Cohen <coheneu@oregonstate.edu>
2025-08-31 11:07:21 -0700

    arch-riscv: add handle tlb/walker to hmmu

--- src/arch/riscv/RiscvHMMU.py
+++ src/arch/riscv/RiscvHMMU.py
@@ -40,6 +40,7 @@ from m5.objects.BaseMMU import BaseMMU
 from m5.objects.PMAChecker import PMAChecker
 from m5.objects.PMP import PMP
 from m5.objects.RiscvTLB import RiscvTLB
+from m5.objects.RiscvHTLB import RiscvHTLB
 from m5.params import *
 
 
@@ -50,13 +51,16 @@ class RiscvHMMU(BaseMMU):
 
     itb = RiscvTLB(entry_type="instruction")
     dtb = RiscvTLB(entry_type="data")
+    htb = Param.RiscvHTLB(RiscvHTLB(entry_type="handle"), "Handle TLB")
     pma_checker = Param.BasePMAChecker(PMAChecker(), "PMA Checker")
     pmp = Param.PMP(PMP(), "Physical Memory Protection Unit")
 
     @classmethod
     def walkerPorts(cls):
-        return ["mmu.itb.walker.port", "mmu.dtb.walker.port"]
+        return ["mmu.itb.walker.port", "mmu.dtb.walker.port", "mmu.htb.walker.port"]
 
     def connectWalkerPorts(self, iport, dport):
         self.itb.walker.port = iport
         self.dtb.walker.port = dport
+        # handle walker port shared with data!!
+        self.htb.walker.port = dport
